6e318e747f47dd439a0cf8da6d9d8d5ca7ae415a
EricXiang1104 <eric.xiang@hli.ubc.ca>
2024-11-17 07:25:00 -0800

    Updated 11.17.24

--- Appdevl/app.R
+++ Appdevl/app.R
@@ -1,54 +1,68 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+
+# Assignment B3. Crea ting a Shiny App
+#install.packages("colourpicker")#install if needed
 
 library(shiny)
 library(tidyverse)
 library(datateachr) 
-cancer_sample
+library(dplyr)
+library(colourpicker)
 
-# Define UI for application that draws a histogram
+
+data("cancer_sample")#cancer_sample from the dataset is used for the web development 
+
+# Define UI for application 
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Cancer Sample  Data"),
+  checkboxInput("variableSort","sort by variable",TRUE),
+  colourInput("col", "Select colour", "purple"),
+  sidebarLayout(
+    sidebarPanel(
+    selectInput("variableInput","Variable",
+               choices=c("radius_mean","texture_mean","perimeter_mean"),
+               selected="radius_mean"),
+    downloadButton("downloadData", "Download Table as CSV")),
+    mainPanel(img(src='cancer.jpg',align = "centre",height='400px',width='400px'),plotOutput("coolplot"),strong(textOutput("numRows")),tableOutput("results")
+              )
     )
-)
+  )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# Define server logic required 
+server <- function(input, output,session) {
+#Select the data based on the user variable selection, if desired, will rearrange the table based on the variable selected.    
+   filtered <-reactive({
+    new_data<-cancer_sample %>%
+    select(ID,diagnosis,input$variableInput)  
+    if (input$variableSort){
+      new_data<-new_data %>% arrange(!!sym(input$variableInput))}
+    return(new_data) })
+#To draw a histogram 
+  output$coolplot <- renderPlot({
+    ggplot(cancer_sample, aes_string(input$variableInput)) +
+      geom_histogram(binwidth = 1, fill=input$col)+
+      labs(title=paste("Histogram of",input$variableInput),x=input$variableInput,y="Count")
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  })
+#table output
+  output$results <- renderTable({filtered()
+  })
+#Row number output
+  output$numRows <- renderText({
+    paste("Number of rows in the table:", nrow(filtered()))
+})
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+#download table  output
+  output$downloadData<-downloadHandler(
+    filename=function(){
+      paste("cancer_table.csv" )},
+    content=function(file){
+      write.csv(filtered (),file)
 }
+  )
+
+}
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+      
