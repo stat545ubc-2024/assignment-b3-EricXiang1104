941dd9917a9d24d607fe1169035e56b1fdec097c
EricXiang1104 <eric.xiang@hli.ubc.ca>
2024-11-19 12:23:07 -0800

    Final Update 11.19.24

--- Appdevl/app.R
+++ Appdevl/app.R
@@ -3,25 +3,31 @@
 #Eric Xiang
 #2024 11.21
 #The goal of this app is to plot histogram for the mean variables in the cancer_sample dataset, 
-#It will also filter out the variable it is plotting and then rearrange the dataset based on the variable of interest.
+#It will also filter out the variable it is plotting and then rearrange the dataset based on the variable of interest, ID and diagnosis status. 
 #It utilizes the cancer_data from the datateachr package
+#The app contains three features 1. Generating the histogram with different colors 2. Generating an interactive table including the selected variables.
+#3.An option to sort the table based on the selected variable of interest and download the table if interested. It will also report the row number of the table. 
+
+#Installation code, not needed if installed
 #install.packages("colourpicker")#install if needed
 #install.packages('rsconnect')#install if needed 
 
-library(shiny)
-library(tidyverse)
-library(datateachr) 
-library(dplyr)
-library(colourpicker)
+library(shiny)# import shiny app for app creation 
+library(tidyverse)#import tidyverse for data analysis 
+library(datateachr) # import datateachr for the data file
+library(dplyr)#dplyr package is for data analysis 
+library(colourpicker)#import colourpicker for changing color of the graph 
+library(DT)#for generating an interactive table 
 
-
-library(rsconnect)
+library(rsconnect)# for publishing the web 
 #publish the web
 #rsconnect::deployApp("C:/Users/Eric Xiang/Desktop/assignment-b3-EricXiang1104/Appdevl")
+
 data("cancer_sample")#cancer_sample from the dataset is used for the web development
 
 
-# Define UI for application 
+# Define UI for application. Allow user to define colors for histogram. The feature is set up for aesthetic purpose but is also useful for colorblind users
+#an cancer cell image is added for aesthetic purpose
 ui <- fluidPage(
   titlePanel("Cancer Sample  Mean Data"),
   checkboxInput("variableSort","sort by variable",TRUE),
@@ -32,36 +38,39 @@ ui <- fluidPage(
                choices=c("radius_mean","texture_mean","perimeter_mean","smoothness_mean","compactness_mean","concavity_mean"),
                selected="radius_mean"),
     downloadButton("downloadData", "Download Table as CSV")),
-    mainPanel(img(src='cancer.jpg',align = "centre",height='400px',width='400px'),plotOutput("coolplot"),strong(textOutput("numRows")),tableOutput("results")
+    mainPanel(img(src='cancer.jpg',align = "centre",height='400px',width='400px'),plotOutput("coolplot"),strong(textOutput("numRows")),DTOutput('tb1'),
               )
     )
   )
 
 # Define server logic required 
 server <- function(input, output,session) {
-#Select the data based on the user variable selection, if desired, will rearrange the table based on the variable selected.    
+#Feature 1: Select the data based on the user variable selection
+#if desired, will rearrange the table based on the variable selected. The feature is useful for generating an organized table downstream    
    filtered <-reactive({
     new_data<-cancer_sample %>%
     select(ID,diagnosis,input$variableInput)  
     if (input$variableSort){
       new_data<-new_data %>% arrange(!!sym(input$variableInput))}
     return(new_data) })
-#To draw a histogram for the selected variable 
+#Feature 2: To draw a histogram for the selected variable to visually understand the distribution of the variable 
   output$coolplot <- renderPlot({
     ggplot(cancer_sample, aes_string(input$variableInput)) +
       geom_histogram(bins = 30 , fill=input$col)+
       labs(title=paste("Histogram of",input$variableInput),x=input$variableInput,y="Count")
 
   })
-#table output
-  output$results <- renderTable({filtered()
-  })
-#Row number output
+#Part of Feature1: Generate a table output. The table is interactive with DT package, making it useful to search up the data
+  
+  output$tb1 = renderDT( datatable(
+    filtered(), options = list(lengthChange = FALSE)))
+  
+#Part of Feature3; Generate the Row number output for the table. Useful to understand how large the dataset is
   output$numRows <- renderText({
     paste("Number of rows in the table:", nrow(filtered()))
 })
 
-#download table  output
+#Feature 3: download table  output as a CSV file useful for future analysis by users
   output$downloadData<-downloadHandler(
     filename=function(){
       paste("cancer_table.csv" )},
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # **Assignment B3 Shiny App Development**
 
-This assignment is creating a web using shiny app. The goal of this app is to plot histogram for the mean variables in the cancer_sample dataset. It will also filter out the variable it is plotting and then rearrange the dataset based on the variable of interest. The app utilizes the cancer_data from the datateachr package
+This assignment is creating a web using shiny app. The goal of this app is to plot a histogram for the mean variables in the cancer_sample dataset (specifically, radius_mean", "texture_mean", "perimeter_mean","smoothness_mean","compactness_mean","concavity_mean). It will also filter out the variable it is plotting and then rearrange the dataset based on the variable of interest. The app utilizes the cancer_data from the datateachr package
 
 The data cancer_sample is from datateachr package, an open source data for education purpose.This cancer_data contains a sample of quantitative features that were calculated from images of nuclei present in fine needle aspiration biopsies of breast masses from patients at the University of Wisconsin Hospital. For this assignment, I am focusing on the mean data.
 
-**Link to the running app:** <https://ericxiang.shinyapps.io/CancerSAmpleMeanData3/>
+The app contains **three features** 1. Generating the histogram with different colors to help visualize the distribution. A histogram can help visualize the distribution of the variable. Different colors is for aesthetic purposes. An image of the cancer cell is added to make the web look pretty (from PharmaTimes online). 2. Generating an interactive table including the selected variables so the user can search up the data they are interested in the dataset. 3.An option to sort the table based on the selected variable of interest and download the table if interested. It will also report the row number of the table. This is to help users clean up the table and save the table for future analysis if needed
+
+**Link to the running app:** <https://ericxiang.shinyapps.io/CancerSampleMean4/>
